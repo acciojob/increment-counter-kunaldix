bb3ea854fa1104fa1b87ef324824b10be6c89660
Kunal Dixit <kunaldixit15049@gmail.com>
2025-04-03 15:41:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const counterElement = document.getElementById('counter');
 const incrementBtn = document.getElementById('incrementBtn');
 
 incrementBtn.addEventListener('click', () => {
-  alert(`${counterValue}`); // Alert shows the current (un-incremented) value
-  counterValue++; // Increment the counter
-  counterElement.textContent = counterValue; // Update the displayed value
+    alert(`${counterValue}`); // Alert shows the current (un-incremented) value
+    counterValue++; // Increment the counter
+    counterElement.textContent = counterValue; // Update the displayed value
 });
